42c600b2544b8acee73c71f0b5448a3b9f9ecfc2
plouiserre <plouiserre@gmail.com>
2026-02-20 11:13:41 +0100

    chiffrement terminé

--- src/Chiffrement.py
+++ src/Chiffrement.py
@@ -1,6 +1,23 @@
 class Chiffrement: 
     def __init__(self):
-        pass
+        self.cle = 0        
 
     def chiffrer(self, texte, cle):
-        return "dqplpwt"
+        self.cle = cle
+        mot_final = ""
+        miniscule_alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        majuscule_alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+        for lettre in texte : 
+            if lettre in miniscule_alphabet : 
+                mot_final += self.__trouver_bonne_lettre(miniscule_alphabet, lettre)
+            elif lettre in majuscule_alphabet : 
+                mot_final += self.__trouver_bonne_lettre(majuscule_alphabet, lettre)
+            else : 
+                mot_final += lettre
+        return mot_final
+    
+    def __trouver_bonne_lettre(self, alphabet, lettre):
+        index =  alphabet.index(lettre)
+        nouvel_index = (index + self.cle) % 26
+        nouvelle_lettre = alphabet[nouvel_index]
+        return nouvelle_lettre
--- tests/test_chiffrement.py
+++ tests/test_chiffrement.py
@@ -2,9 +2,32 @@ import unittest
 from src.Chiffrement import Chiffrement
 
 class ChiffrementTest(unittest.TestCase):
-    def test_chiffrement_mot_simple(self):
+    def test_chiffrement_mot_cle_simple(self):
         chiffrement = Chiffrement()
         
         mot_chiffre = chiffrement.chiffrer("bonjour", 2)
 
-        self.assertEqual("dqplpwt", mot_chiffre)
+        self.assertEqual("dqplqwt", mot_chiffre)
+
+    def test_chiffrement_mot_cle_complexe(self):
+        chiffrement = Chiffrement()
+        
+        mot_chiffre = chiffrement.chiffrer("bonjour", 8)
+
+        self.assertEqual("jwvrwcz", mot_chiffre)
+
+
+    def test_chiffrement_deux_mots_cle_simple(self):
+        chiffrement = Chiffrement()
+        
+        mot_chiffre = chiffrement.chiffrer("bonjour madame", 2)
+
+        self.assertEqual("dqplqwt ocfcog", mot_chiffre)
+
+    
+    def test_chiffrement_majuscule(self):
+        chiffrement = Chiffrement()
+
+        mot_chiffre = chiffrement.chiffrer("Bonjour", 2)
+
+        self.assertEqual("Dqplqwt", mot_chiffre)
