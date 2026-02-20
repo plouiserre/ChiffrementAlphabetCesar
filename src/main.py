7e3efe2985d17d76e4f0cf546682b9b4998a486c
plouiserre <plouiserre@gmail.com>
2026-02-20 11:30:31 +0100

    read dico_fr txt

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("looool")
+import os 
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+file = os.path.join(base_dir, "donnees\\dico_fr.txt")
+with open(file, "r", encoding="latin1") as f:
+    contenu_fichier = f.read()
+    print(contenu_fichier)
